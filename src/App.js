b2fbd573f2bc823004efe7468d258c7aeeab30ff
diego1321 <98927286+diego1321@users.noreply.github.com>
2022-08-19 16:16:53 -0300

    componente datos personales (#2)
    * se agrego un nuevo componente con datos basicos y foto personal
    
    * se corrigieron nombres y espacios y se agrego el cambio de tema claro/oscuro

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import ButtonAppBar from "./componentes/layout";
+import Layout from "./componentes/layout/Layout";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { useState } from "react";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+});
 
 function App() {
+  const [mode, setMode] = useState("claro")
+
   return (
-    <div className="App">
-     <ButtonAppBar/>
-    </div>
-  );
+    <ThemeProvider theme={mode === "claro" ? lightTheme : darkTheme}>
+      <CssBaseline />
+      <div className="App">
+     <Layout mode={mode} setMode={setMode}/>
+     </div>
+    </ThemeProvider>
+    );
 }
 
 export default App;
---
+++ src/componentes/datos-personales/DatosPersonales.jsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { Avatar, CardHeader } from '@mui/material';
+
+export default function DatosPersonales() {
+  return (
+    <Card sx={{ margin:"20px"}} elevation={6}     >
+        <CardContent>
+            <CardHeader avatar={<Avatar src="foto-cv.jpeg"/>}/>
+            <Typography gutterBottom variant="h5" component="div">
+           03 de Diciembre del 2003. Fusagasuga, Colombia.
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+          Soy un programador novato que esta empezando con sus primeros proyectos y le gusta investigar. Estoy buscando mi primera experiencia como programador y la idea es estudiar algo relacionado a programación o sistemas.
+          </Typography>
+        </CardContent>
+    </Card>
+  );
+}
