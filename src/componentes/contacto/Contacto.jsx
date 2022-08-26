3816e0416d2e8ff0d6b3bc9aa7e8a24f7b284f84
diego1321 <98927286+diego1321@users.noreply.github.com>
2022-08-26 17:19:15 -0300

    se agregaron espacios para hacer contacto junto con un boton (aun sin funcionalidad) (#5)
    * se agregaron espacios para hacer contacto junto con un boton (aun sin funcionalidad)
    
    * se cambio el titulo de contacto

---
+++ src/componentes/contacto/Contacto.jsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import { Typography } from '@mui/material';
+
+export default function Contacto() {
+    return (
+      <Card sx={{ margin:"20px"}} elevation={6} >
+        <CardContent>
+        <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+          <Typography variant="h5" component="div">
+            Contacto
+          </Typography>
+          <TextField id="filled-basic" label="Nombre" variant="filled" />
+          <TextField id="filled-basic" label="Apellido" variant="filled" />
+          <TextField id="filled-basic" label="Email" variant="filled" />
+          <TextField id="filled-basic" label="Asunto" variant="filled" />
+          <TextField id="filled-basic" label="Mensaje" variant="filled" />
+         </Box>
+        </CardContent>
+        <CardActions>
+          <Button size="small" variant="contained" >Enviar</Button>
+        </CardActions>
+      </Card>
+    );
+  }
+  
